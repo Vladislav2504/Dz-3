fff5355faf584b45f05cbabd0324a66b393da22e
DZ-2 <Fomin.vlad25@mail.ru>
2022-05-11 21:19:46 +0300

    https://github.com/Vladislav2504/Dz-3.git

---
+++ Dz_3.java
@@ -0,0 +1,138 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Dz_3 {
+    public static void main(String[] args) {
+        // 1. Написать программу для вывода названия поры года по номеру месяца.
+        // С использование switch.case.
+
+        //        int num;
+        //        Scanner scanner = new Scanner(System.in);
+        //        num = scanner.nextInt();
+        //
+        //        switch (num) {
+        //            case 12, 1, 2:
+        //                System.out.println("Пора года - зима.");
+        //                break;
+        //
+        //            case 3, 4, 5:
+        //                System.out.println("Пора года - весна.");
+        //                break;
+        //
+        //            case 6, 7, 8:
+        //                System.out.println("Пора года - лето.");
+        //                break;
+        //
+        //            case 9, 10, 11:
+        //                System.out.println("Пора года - осень.");
+        //                break; }
+
+
+            // 2. Написать программу для вывода названия поры года по номеру месяца.
+            // С использование if-else-if.
+
+            //         int num;
+            //         Scanner scanner=new Scanner (System.in);
+            //         num = scanner.nextInt();
+            //
+            //                if ((num >= 3) && (num <= 5)) {
+            //                    System.out.println("Пора года - весна.");
+            //                }
+            //                if ((num>=6) && (num<=8)) {
+            //                    System.out.println("Пора года - лето.");
+            //                }
+            //               if ((num>=9) && (num<=11)) {
+            //                   System.out.println("Пора года - осень.");
+            //               }
+            //               if ((num>=1) && (num<=2)) {
+            //                   System.out.println("Пора года - зима.");
+            //               }
+            //               if ((num>=12) && (num<=12))
+            //                   System.out.println("Пора года - зима.");
+
+
+            // 3. Напишите программу, которая будет принимать на вход число и на выход будет выводить сообщение четное число или нет.
+
+            //        int num;
+            //        Scanner scanner=new Scanner (System.in);
+            //        num= scanner.nextInt();
+            //        if (num%2==0)
+            //            System.out.println("Четное.");
+            //        else
+            //            System.out.println("Нечетное.");
+
+
+        // 4. Для введенного числа t (температура на улице) вывести
+
+        //        int t = 7;
+        //
+        //        {
+        //            if (t > -5)
+        //                System.out.println("Тепло.");
+        //
+        //        }
+        //        {
+        //            if (-5 >= t - 20)
+        //                System.out.println("Нормально.");
+        //        }
+        //        {
+        //            if (-20>=t)
+        //                System.out.println("Холодно");
+        //        }
+
+        // 5. По введенному номеру определить цвет радуги.
+
+        //        int num;
+        //                Scanner scanner = new Scanner(System.in);
+        //                num = scanner.nextInt();
+        //
+        //        switch (num) {
+        //            case 1:
+        //                System.out.println("Красный");
+        //                break;
+        //
+        //            case 2:
+        //                System.out.println("Оранжевый");
+        //                break;
+        //
+        //            case 3:
+        //                System.out.println("Желтый");
+        //                break;
+        //
+        //            case 4:
+        //                System.out.println("Зеленый");
+        //                break;
+        //
+        //            case 5:
+        //                System.out.println("Голубой");
+        //                break;
+        //
+        //            case 6:
+        //                System.out.println("Синий");
+        //                break;
+        //
+        //            case 7:
+        //                System.out.println("Фиолетовый");
+        //                break;
+        //        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+
+    }
+
+
+
